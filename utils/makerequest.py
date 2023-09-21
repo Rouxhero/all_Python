06b6f6df21d5de141e2bee46cb738d7bf6f309a1
Rouxhero <70963483+Rouxhero@users.noreply.github.com>
2023-09-21 11:47:06 +0200

    Update makerequest.py

--- utils/makerequest.py
+++ utils/makerequest.py
@@ -36,12 +36,12 @@ match len(sys.argv):
         
 file = {"file":open("vehicule.json","rb")}
 data = {
-    "username":"leo",
-    "password":"leof",
-    "email":"leolvcdb@gmail.com",
-    "host":"ftp.givemecoffee.ninja",
+    "username":"",
+    "password":"",
+    "email":"",
+    "host":"",
     "port":"21",
-    "name":"directory_A"
+    "name":""
 }
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
